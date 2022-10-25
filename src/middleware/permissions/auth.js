dfb33502d31a1b320224632fbe58a9bd5673c60c
Shaun Stocker <s.stocker04@outlook.com>
2022-10-25 22:06:15 +0100

    Added permissions and error messages

---
+++ src/middleware/permissions/auth.js
@@ -0,0 +1,52 @@
+const { ROLES } = require('./roles');
+
+function isAdmin(user) {
+    return (user.role === ROLES.ADMIN);
+};
+
+function isOwner(user, item) {
+    return (user.id === item.userId.toString());
+};
+
+// This is for users only
+function isOwnerOrAdmin(user, userId) {
+    return (user.role === ROLES.ADMIN || user.id === userId);
+};
+
+function isOwnerOfObjOrAdmin(user, item) {
+    return ( user.role === ROLES.ADMIN || item.userId.toString() === user.id);
+};
+
+// function isAdmin(req, res, next) {
+//     if (req.user.role !== ROLES.ADMIN) {
+//         return res.status(403).send('Only an administrator can perform this action');
+//     }
+//     next();
+// }
+
+// function isOwnerOrAdmin(req, res, next) {
+//     const { user } = req;
+//     const userId = req.params.id;
+//     // console.log("Request:", req);
+//     if ( user.role === ROLES.ADMIN || user.id === userId) {
+//         next();
+//     } else {
+//         return res.status(403).send('Only the owner of this account or an administrator can perform this action');
+//     }
+// };
+
+// function isOwnerOrAdminOfObj(user, item) {
+//     // console.log("Request:", req);
+//     if ( user.role === ROLES.ADMIN || item.userId === user.id) {
+//         next();
+//     } else {
+//         return res.status(403).send('Only the owner of this object or an administrator can perform this action');
+//     }
+// };
+
+module.exports = {
+    isAdmin,
+    isOwner,
+    isOwnerOrAdmin,
+    isOwnerOfObjOrAdmin,
+};
---
+++ src/utils/error_messages.js
@@ -0,0 +1,7 @@
+const ERRORS = {
+    NOT_ADMIN: 'Only an administrator can perform this action',
+    NOT_OWNER: 'Only the owner of this object can perform this action',
+    NOT_ADMIN_OR_OWNER: 'Only the owner of this object or an administrator can perform this action'
+}
+
+module.exports = { ERRORS };
